3d3750751d65e48670b0138ae04c787521845bca
Wurrd <matthanson2005@gmail.com>
2025-07-16 18:21:21 -0700

    Add static export config for GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/wurrd.github.io/',
+  assetPrefix: '/wurrd.github.io/',
+}
+
+module.exports = nextConfig
