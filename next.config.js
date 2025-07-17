dae11dc51b784bb4bf3cace468a6bdc0de913bb5
Wurrd <matthanson2005@gmail.com>
2025-07-16 18:25:13 -0700

    Fix basePath (no trailing slash)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/wurrd.github.io/',
-  assetPrefix: '/wurrd.github.io/',
 }
 
 module.exports = nextConfig
