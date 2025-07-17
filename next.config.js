d1e7e451aed015cb8f5960282084d9179004d842
Wurrd <matthanson2005@gmail.com>
2025-07-17 14:34:19 -0700

    1000th revert because npm run doesn't work

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/wurrd.github.io' : '',
-  assetPrefix: isProd ? '/wurrd.github.io/' : '',
-};
+}
