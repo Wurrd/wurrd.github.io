c3709e9ba24c222d40fffdd6166fd2195c2a3281
Wurrd <matthanson2005@gmail.com>
2025-07-17 15:41:21 -0700

    message

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      return <link rel="icon" href="/favicon.ico" />;
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
