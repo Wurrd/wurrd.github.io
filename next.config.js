b1595b4cc741fd980826e5d386295190f342eb1d
Wurrd <matthanson2005@gmail.com>
2025-07-17 14:30:54 -0700

    Deploy static site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   output: 'export',
   trailingSlash: true,
-}
+  basePath: isProd ? '/wurrd.github.io' : '',
+  assetPrefix: isProd ? '/wurrd.github.io/' : '',
+};
