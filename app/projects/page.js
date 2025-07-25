f7b71d239fc017a1bb1e845f01ff5d16a7684296
Wurrd <matthanson2005@gmail.com>
2025-07-25 13:52:25 -0700

    code: comments

--- app/projects/page.js
+++ app/projects/page.js
@@ -10,6 +10,14 @@ export default function Projects() {
             <p className="text-gray-800 mt-2">
               <a href="https://github.com/Googolplexic/LoL-discord-bot" target="_blank">League of Legends Discord Bot</a>
             </p>
+            {/*
+            <a href="https://github.com/TMZero-c/CMPT371Project" target="_blank">Blend In: Game</a>
+            <a href="https://github.com/TMZero-c/CMPT371Project" target="_blank">Blend In: Game</a>
+            <a href="https://github.com/CMPT-276-SUMMER-2025/final-project-21-spring" target="_blank">CloudCue</a>
+            <a href="https://github.com/Googolplexic/beSquare" target="_blank">Adobe Express Add-on</a>
+            <a href="https://github.com/MACHI-NE/MACHI-NE-" target="_blank">Emergency Event Reporter</a>
+            <a href="https://github.com/Marcetho/salmon-run" target="_blank">Salmon Run: Game</a>
+            */}
           </div>
         ))}
       </div>
