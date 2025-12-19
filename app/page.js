cfe6315e8a6085a616cf1248664a16d109b51548
Wurrd <matthanson2005@gmail.com>
2025-12-18 16:32:42 -0800

    feat: fully removed photo

--- app/page.js
+++ app/page.js
@@ -5,7 +5,6 @@ export default function Home() {
     <Layout activePage="home">
       <div className="flex space-x-6 items-start">
         <div>
-          <img src="/photo.jpg" alt="Passport Photo" className="w-48 h-auto" />
         </div>
         <p className="text-gray-800 text-base leading-relaxed max-w-prose">
           I’m Howard Jin, 3rd year student at SFU.
