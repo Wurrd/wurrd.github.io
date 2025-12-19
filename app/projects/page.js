c29b3c31ca0423e784e991256cbd786655c0c644
Wurrd <matthanson2005@gmail.com>
2025-12-18 17:27:34 -0800

    feat: testing different project blocks

--- app/projects/page.js
+++ app/projects/page.js
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <Layout activePage="Projects">
       <div className="grid grid-cols-2 gap-6">
-        {Array.from({ length: 1 }).map((_, index) => (
+        {Array.from({ length: 6 }).map((_, index) => (
           <div key={index} className="flex space-x-4 items-start">
             <div className="bg-red-600 w-24 h-24 rounded-md" />
             <p className="text-gray-800 mt-2">
