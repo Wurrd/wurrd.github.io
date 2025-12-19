3b801521e9e6a69395eda5cb73ca87c2cc85e774
Wurrd <matthanson2005@gmail.com>
2025-12-18 17:25:23 -0800

    message

--- app/projects/page.js
+++ app/projects/page.js
@@ -9,11 +9,20 @@ export default function Projects() {
             <div className="bg-red-600 w-24 h-24 rounded-md" />
             <p className="text-gray-800 mt-2">
               <a href="https://github.com/Googolplexic/LoL-discord-bot" target="_blank">League of Legends Discord Bot</a>
+            </p>
+            <p className="text-gray-800 mt-2">
               <a href="https://github.com/TMZero-c/CMPT371Project" target="_blank">Blend In: Game</a>
-              <a href="https://github.com/TMZero-c/CMPT371Project" target="_blank">Blend In: Game</a>
+            </p>
+            <p className="text-gray-800 mt-2">
               <a href="https://github.com/CMPT-276-SUMMER-2025/final-project-21-spring" target="_blank">CloudCue</a>
+            </p>
+            <p className="text-gray-800 mt-2">
               <a href="https://github.com/Googolplexic/beSquare" target="_blank">Adobe Express Add-on</a>
+            </p>
+            <p className="text-gray-800 mt-2">
               <a href="https://github.com/MACHI-NE/MACHI-NE-" target="_blank">Emergency Event Reporter</a>
+            </p>
+            <p className="text-gray-800 mt-2">
               <a href="https://github.com/Marcetho/salmon-run" target="_blank">Salmon Run: Game</a>
             </p>
           </div>
